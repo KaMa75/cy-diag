7155b9f734cbf451c5b7cb7101d6171fd352d9f5
KaMa75 <karolmarszalek@o2.pl>
2021-06-18 10:36:28 +0200

    test client list pagination

--- cypress/integration/specs/facility-coordinator/clientsList.spec.js
+++ cypress/integration/specs/facility-coordinator/clientsList.spec.js
@@ -10,21 +10,16 @@ import { clientsList } from '../../page-objects/clientsList.po';
 import { headBar } from '../../page-objects/headBar.po';
 import { pagination } from '../../page-objects/pagination.po';
 
-let userId;
-
 describe('Test clients list - Facility Coordinator', () => {
     
     before('Log In to app', () => {
         cy.logInToAdminApp(users.facCoordinator);
+        // cy.saveLocalStorage();
         headBar.header.then(($header) => {
             if($header.text() !== headbarHeaders.clients) {
                 navigationBar.goToClients();
             }
         })
-
-        cy.getLocalStorage('diagmaticCurrentUserId').then(txt => {
-            userId = txt;
-        });
     });
     
     beforeEach('Read localstorage', () => {
@@ -32,24 +27,17 @@ describe('Test clients list - Facility Coordinator', () => {
     });
 
 
-    afterEach('Save localstorage', () => {
-        cy.saveLocalStorage();
-    });
-
     it(`Should be ${cardsHeaders.clientsList} header`, () => {
         clientsList.cardHeader.should('have.text', cardsHeaders.clientsList);
     });
 
     
     it('Should be default pagination value after login - 10 rows', () => {
-        cy.getLocalStorage('diagmaticCurrentUserId').then(txt => {
-            console.log(txt)
-        });
         pagination.rowsInput.should('have.value', '10');
     });
 
 
-    it('Should be default pagination page number after card open - 1\'st page, prev buttons are diabled', () => {
+    it('Should be default pagination page number after card open - 1\'st page, prev/first buttons are diabled', () => {
 
         pagination.pagCaption.then(($caption) => {
             expect(pagination.getPagCaptionInfo($caption).firstElement).to.equal(1);
@@ -63,7 +51,7 @@ describe('Test clients list - Facility Coordinator', () => {
 
     });
 
-    it('Should be disabled next buttons at last page', () => {
+    it('Should be disabled next/last buttons at last page', () => {
         cy.logInToAdminApp(users.facCoordinator);
 
         pagination.pagCaption.then(($caption) => {
